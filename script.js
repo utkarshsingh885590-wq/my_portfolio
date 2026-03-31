5b800a8a0303d3771466dc51b81b395ffac6524e
utkarshsingh885590-wq <utkarshsingh885590@gmail.com>
2026-03-31 18:48:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. Gemini AI Configuration
-const GEMINI_KEY = "AIzaSyCNxRTF_fe2ygGboZLrGknOd30a61vTPrQ";
+const GEMINI_KEY = "AIzaSyBSROwkYwyG0Dzz8-WKSzjak60XYz-fRNA";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_KEY}`;
 
 // 2. Toggle Chat Window
